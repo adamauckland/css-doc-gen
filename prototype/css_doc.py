43859bcc156ad425b66027c0ee12ea053db3b315
Adam Auckland <adamauckland@gmail.com>
2013-01-31 19:34:02 +0000

    first working prototype. Does not recurse

--- prototype/css_doc.py
+++ prototype/css_doc.py
@@ -3,16 +3,139 @@ import os.path
 import sys
 
 
+class CssItem(object):
+    def __init__(self):
+        self.selector = None
+        self.comments = {}
+        self.definition = None
+        self.atom = None
+
+    def __str__(self):
+        return 'Element: %s\n%s\n%s\nAtom:\n%s' % (self.selector, self.comments, self.definition, self.atom)
+
+
+class CssChomper(object):
+    def __init__(self, chomp_stack):
+        self.chomp_stack = chomp_stack
+        self.reset()
+
+    def reset(self):
+        self.comment_buffer = {}
+        self.buffer = []
+        self.entry_count = 0
+        self.in_comments = False
+        self.selector = []
+        self.definition = []
+        self.entered_definition = False
+        self.finished_selector_buffer = []
+
+    def __repr__(self):
+        return repr(self.comments)
+
+    def parse(self, data):
+        for loop_character in data:
+            self.chomp(loop_character)
+
+    def chomp(self, character):
+        self.buffer.append(character)
+
+        is_control_character = False
+
+        if character == '{':
+            is_control_character = True
+            self.entered_definition = True
+            self.entry_count += 1
+
+        if character == '}':
+            #
+            # Hack to add final }
+            #
+            self.definition.append(character)
+
+            is_control_character = True
+            self.entry_count -= 1
+            #
+            # Reached end
+            #
+            if self.entry_count == 0:
+                self.output()
+
+        if not is_control_character:
+            if not self.entered_definition:
+                self.selector.append(character)
+
+        if self.entered_definition:
+            self.definition.append(character)
+
+    def output(self):
+        self.selector_text = ''.join(self.selector)
+        self.definition_text = ''.join(self.definition)
+
+        self.look_for_comments()
+
+        css_item = CssItem()
+        css_item.atom = ''.join(self.buffer)
+        css_item.selector = '\n'.join(self.finished_selector_buffer)
+        css_item.comments = self.comment_buffer
+        css_item.definition = self.definition_text
+
+        self.chomp_stack.append(css_item)
+
+        #
+        # look for anything inside me
+        #
+        #css_object = CssChomper(self.chomp_stack)
+        #css_object.parse(self.definition_text)
+
+        #print('\n\nselector %s' % '\n'.join(self.finished_selector_buffer))
+        #print('Comment buffer: %s' % self.comment_buffer)
+        #print('definition  { %s }' % self.definition_text)
+
+    def look_for_comments(self):
+        for line in self.selector_text.split('\n'):
+            comment_index = line.find('//')
+            #
+            # Found a comment
+            #
+            if comment_index > -1:
+                line = line[comment_index + 2:]
+                at_index = line.find('@')
+                if at_index != -1:
+                    at_split = line.split('@')
+                    at_key = at_split[1].split(' ')[0].strip()
+                    at_value = line[at_index + len(at_key) + 1:].strip()
+
+                    self.comment_buffer[at_key] = at_value
+            else:
+                self.finished_selector_buffer.append(line)
+
+
 class ParseReader(object):
     def __init__(self):
         self.comment_buffer = {}
         self.css_match_buffer = []
+        self.css_objects = []
+
+    def parse_doc(self, filename, data):
+        self.log(filename)
+        chomp_stack = []
+
+        css_object = CssChomper(chomp_stack)
+        css_object.parse(data)
+
+        for loop_item in chomp_stack:
+            if len(loop_item.comments) > 0:
+                print(loop_item)
+
+        #self.log(data)
+
 
     def parse_line(self, original_line):
-        self.log('Parse: %s' % original_line)
+        #self.log('Parse: %s' % original_line)
 
         line = original_line
         comment_index = line.find('//')
+        open_brace_index = line.find('{')
 
         #
         # Found a comment
@@ -25,8 +148,18 @@ class ParseReader(object):
                 at_key = at_split[1].split(' ')[0].strip()
                 at_value = line[at_index + len(at_key) + 1:].strip()
 
-                self.log('\t\t%s --> %s' % (at_key, at_value))
+                #self.log('\t\t%s --> %s' % (at_key, at_value))
                 self.comment_buffer[at_key] = at_value
+
+        #
+        # Push output onto stack
+        if line.find('}') != -1:
+            if len(self.comment_buffer) > 0:
+                new_object = CssObject()
+                new_object.comments = self.comment_buffer
+                self.css_objects.append(new_object)
+
+            self.comment_buffer = {}
 
 
 class CssDoc(object):
@@ -57,8 +190,7 @@ class CssDoc(object):
     def parse_file(self, loop_key, loop_data):
         parser = ParseReader()
         parser.log = self.log
-        for loop_line in loop_data.split('\n'):
-            parser.parse_line(loop_line)
+        parser.parse_doc(loop_key, loop_data)
 
 
 def log(text):
